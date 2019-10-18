a2cb5478a38995d668e4f2c442b64fb7fd15af86
kazushi sakashita <kazusi@hotmail.co.jp>
2019-10-19 03:57:54 +0900

    [add] テキスト送り機能を追加

--- UnityProject/Exit With/Assets/ExitWith/Scripts/TextAsset.cs
+++ UnityProject/Exit With/Assets/ExitWith/Scripts/TextAsset.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
+/// <summary>
+/// 一連単位のテキストアセット
+/// 
+/// プレイヤーの行動と行動の間で1アセット,感覚としては節に近い?
+/// e.g. 探索 => 描写(アセット) => 移動 => 描写(アセット) => ....
+/// ここで2つの描写は、別々のアセットに入る
+/// </summary>
 [CreateAssetMenu(menuName = "ScriptableObject/TextAsset")]
 public class TextAsset : ScriptableObject
 {
-    [SerializeField, TextArea] private string text;
+    [SerializeField, TextArea(1,100)] private string text;
     public string[] SplitedText;
 
     public void OnEnable()
--- UnityProject/Exit With/Assets/ExitWith/Scripts/TextWindow.cs
+++ UnityProject/Exit With/Assets/ExitWith/Scripts/TextWindow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UniRx.Async;
 using TMPro;
 
 public class TextWindow : MonoBehaviour,IPointerClickHandler
@@ -11,6 +12,8 @@ public class TextWindow : MonoBehaviour,IPointerClickHandler
     [SerializeField] private TMP_Text text;
     private TextAsset textAsset;
     private int cnt = 0;
+    private bool nowFeeding = false; //現在テキスト送り中か
+    private bool skipFeeding; //テキスト送りをスキップして即座に文を出す
 
     public void SetText(TextAsset asset)
     {
@@ -21,10 +24,37 @@ public class TextWindow : MonoBehaviour,IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (cnt >= textAsset.SplitedText.Length)
-            return;
+        TextFeed();
+    }
 
+    private void TextFeed()
+    {
+        if (nowFeeding)
+        {
+            skipFeeding = true;
+            return;
+        }
+
+        if (cnt >= textAsset.SplitedText.Length)
+        {
+            return;
+        }
+
+        nowFeeding = true;
+        TextFeedAsync();
+    }
+
+    private async UniTask TextFeedAsync()
+    {
+        text.text = "";
+        for (int i = 0; i < textAsset.SplitedText[cnt].Length && !skipFeeding; i++)
+        {
+            await UniTask.Delay(1);
+            text.text += textAsset.SplitedText[cnt][i];
+        }
         text.text = textAsset.SplitedText[cnt];
+        skipFeeding = false;
+        nowFeeding = false;
         cnt++;
     }
 }
