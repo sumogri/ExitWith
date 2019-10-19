93d4cca13a8d1a1a760e59341f33bf85f5708644
kazushi sakashita <kazusi@hotmail.co.jp>
2019-10-20 00:22:00 +0900

    [add] 1Fマップを仮置き

---
+++ UnityProject/Exit With/Assets/ExitWith/Scripts/ActionWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 行動の選択肢ウィンドウ
+/// </summary>
+public class ActionWindow : MonoBehaviour
+{
+    [SerializeField] private GameObject actButtons;
+    [SerializeField] private Button find;
+    [SerializeField] private Button move;
+    [SerializeField] private Button item;
+    [SerializeField] private Button ritual;
+    [SerializeField] private GameObject yesNoButtons;
+    [SerializeField] private Button yes;
+    [SerializeField] private Button no;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActionActivate()
+    {
+        actButtons.SetActive(true);
+    }
+
+    public void YesNoActivate()
+    {
+        yesNoButtons.SetActive(true);
+    }
+
+    public void ritualActivate()
+    {
+        ritual.gameObject.SetActive(true);
+    }
+}
---
+++ UnityProject/Exit With/Assets/ExitWith/Scripts/PlayerState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    public int HP { get; set; } = 0;
+    public int Plase { get; set; } = 0;
+    public List<int> Items { get; set; } = new List<int>();
+}
